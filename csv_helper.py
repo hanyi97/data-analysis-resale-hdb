434d1dd3a78baaa14951aa6340b70e0e21eae249
HanYi <nghanyi1997@gmail.com>
2020-10-06 11:06:45 +0800

    Added docstrings to module

--- csv_helper.py
+++ csv_helper.py
@@ -1,16 +1,30 @@
-import csv
+"""This is a module to handle the csv data
+It includes two functions
+One function to retrieve data as a list
+One function to retrieve data as dictionary
+"""
 
+import csv
 CONST_filename = 'resale_flat_prices.csv'
 
 
-# Reads data from CSV and returns a list
-# Note that first item in list are the columns of the dataset
 def get_data():
+    """Reads data from CSV and returns a list of each row
+    Note: first item in list are the columns of the dataset
+
+    Returns:
+    list: list of all rows in csv file including header row
+    """
     with open(CONST_filename, 'r', encoding='utf-8-sig') as csv_file:
         return list(csv.reader(csv_file, delimiter=','))
 
 
-# Reads data from CSV and returns it as a list of dictionaries
 def get_dict_data():
+    """Reads data from CSV and returns it as a list of dictionaries of each row
+    {'header': 'value'}
+
+    Returns:
+    list: list of dictionaries of each row of data
+    """
     with open(CONST_filename, 'r', encoding='utf-8-sig') as csv_file:
         return list(csv.DictReader(csv_file, delimiter=','))
