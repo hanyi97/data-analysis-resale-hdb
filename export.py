c38b4f5e23030f0aa28ab03d2427959b5d8f47fc
HanYi <nghanyi1997@gmail.com>
2020-10-07 20:53:58 +0800

    Modified export function but still not completed.

--- export.py
+++ export.py
@@ -4,5 +4,12 @@ from fpdf import FPDF
 
 
 def export_to_pdf():
-    pdf = FPDF()
+    pdf = FPDF('L')
     pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(40, 10, 'Hello World!', 1)
+    pdf.cell(60, 10, 'Testing.', 0, 1, 'C')
+    pdf.image('bargraph.png', x=None, y=None, w=0, h=0, type='', link='')
+    pdf.output('test.pdf', 'F')
+
+export_to_pdf()
