5ef8f6e6986b5f859e41e0a6d796e8991036dea3
HanYi <nghanyi1997@gmail.com>
2020-10-07 21:06:30 +0800

    Modified path to save pdf

--- export.py
+++ export.py
@@ -9,7 +9,7 @@ def export_to_pdf():
     pdf.set_font('Arial', 'B', 16)
     pdf.cell(40, 10, 'Hello World!', 1)
     pdf.cell(60, 10, 'Testing.', 0, 1, 'C')
-    pdf.image('bargraph.png', x=None, y=None, w=0, h=0, type='', link='')
+    pdf.image('resources/bargraph.png', x=None, y=None, w=0, h=0, type='', link='')
     pdf.output('test.pdf', 'F')
 
 export_to_pdf()
