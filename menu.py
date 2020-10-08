b189824b803ee4e0b382332b484b1be6a9e39028
Belle Sim <bellesimbeier@gmail.com>
2020-10-08 13:35:04 +0800

    Added link to buttons and added back button to each page

--- menu.py
+++ menu.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 
 LARGE_FONT = ("Open Sans", 30)
-NORM_FONT = ("Helvetica", 10)
-SMALL_FONT = ("Helvetica", 8)
+NORM_FONT = ("Open Sans", 20)
+SMALL_FONT = ("Open Sans", 15)
 
 
 class WelcomeWindow(tk.Tk):
@@ -13,9 +13,20 @@ class WelcomeWindow(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         self.frames = {}
-        frame = WelcomePage(container, self)
-        self.frames[WelcomePage] = frame
-        frame.grid(row=0, column=0, sticky="nsew")
+        # frame = WelcomePage(container, self)
+        # self.frames[WelcomePage] = frame
+        # frame.grid(row=0, column=0, sticky="nsew")
+        # self.show_frame(WelcomePage)
+
+        for F in (WelcomePage, ViewCharts,ViewSummary):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+            frame = F(container, controller=self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
         self.show_frame(WelcomePage)
 
     def show_frame(self, cont):
@@ -26,21 +37,41 @@ class WelcomeWindow(tk.Tk):
 class WelcomePage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.createLabels()
+        label = tk.Label(text="HDB Resale Flats Analyser", font=LARGE_FONT)
+        label.pack()
         self.createButtons(controller)
 
-    def createLabels(self):
-        label = tk.Label(text="HDB Resale Flats Analyzer", font=LARGE_FONT)
-        label.pack(pady=0, padx=10)
-
     def createButtons(self, controller):
-        chartsBTN = tk.Button(self, text="View Charts", height=5, width=30)
+        chartsBTN = tk.Button(self, text="View Charts", height=5, width=30,
+                              command=lambda: controller.show_frame(ViewCharts))
         chartsBTN.pack(pady=0, padx=10)
-        summaryBTN = tk.Button(self, text="View Summary",height=5, width=30)
+        summaryBTN = tk.Button(self, text="View Summary", height=5, width=30,
+                               command=lambda: controller.show_frame(ViewSummary))
         summaryBTN.pack(pady=0, padx=10)
 
 
+class ViewCharts(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Analyse Resale Flats by Region", font=NORM_FONT)
+        label.pack(pady=10, padx=10)
+
+        backbutton = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(WelcomePage))
+        backbutton.pack(pady=10, padx=10)
+
+
+class ViewSummary(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Resale Flats Summary", font=NORM_FONT)
+        label.pack(pady=10, padx=10)
+
+        backbutton = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(WelcomePage))
+        backbutton.pack()
+
+
+# Opens the welcome window as soon as this program runs
 app = WelcomeWindow()
-app.title("ICT1002 Project")
-app.geometry("500x600")
+app.title("HDB Resale Flats Analyser")
+app.geometry("600x600")
 app.mainloop()
