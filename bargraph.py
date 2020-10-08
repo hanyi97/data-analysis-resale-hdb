9d5dd7ad5b2bffe9124321b6f03000b60c057db8
HanYi <nghanyi1997@gmail.com>
2020-10-08 20:37:24 +0800

    Modified code to use panda library

--- bargraph.py
+++ bargraph.py
@@ -1,98 +1,57 @@
 """This is a module to plot bar graph
 Based on average resale prices for different flat types.
-Can be filtered by different town and year"""
+Can be filtered by town and year
+Can export graph as png image"""
 
 import csv_helper
-import numpy as np
 from matplotlib import pyplot as plt
 
 
-def add_flat_types():
-    """Create a dictionary with all the flat types
-    Assume that there are only these range of flat types
-
-    Returns:
-    dictionary: flat type as key and empty list as value
-    """
-    return {'1 ROOM': [], '2 ROOM': [], '3 ROOM': [], '4 ROOM': [], '5 ROOM': [], 'EXECUTIVE': [],
-            'MULTI-GENERATION': []}
-
-
-def filter_data(town, year):
-    """Grouping all resale prices based on flat type
-    Optional to filter by town
+def get_filtered_data(town='', year=''):
+    """Group all resale prices based on flat type
+    Optional to filter by town or year (or both)
 
     Parameters:
-    town (str): town can be empty if no filtering is selected
-    year (str): year can be empty if no filtering is selected
+    town (str): filter data by town (optional)
+    year (str): filter data by year (optional)
 
     Returns:
-    dictionary: flat type as key and list of resale prices based on flat type as value
+    dataframe: dataframe of filtered results
     """
-    data = add_flat_types()
-    csv_data = csv_helper.get_dict_data()
-
-    for line in csv_data:
-        flat_type = line['flat_type']
-        resale_price = float(line['resale_price'])
-
-        # Filter by both town and year
-        if town != '' and year != '':
-            if line['town'].upper() == town and line['year'] == year:
-                data[flat_type].append(resale_price)
-        # Filter by town only
-        elif town != '' and year == '':
-            if line['town'].upper() == town:
-                data[flat_type].append(resale_price)
-        # Filter by year only
-        elif town == '' and year != '':
-            if line['year'] == year:
-                data[flat_type].append(resale_price)
-        # No filter
-        else:
-            data[flat_type].append(resale_price)
-    return data
-
-
-def get_data(town, year):
-    """Calculate average for each flat type
-
-    Parameters:
-    town (str): town can be empty if no filtering is selected
-    year (str): year can be empty if no filtering is selected
-
-    Returns:
-    dictionary: flat type as the key and average resale price as value
-    """
-
-    data = filter_data(town, year)
-    for item in data:
-        if len(data[item]) == 0:
-            data[item].append(0)
-        data[item] = round(sum(data[item]) / len(data[item]), 2)
-    return data
+    df = csv_helper.get_dataframe()
+    if town != '' and year != '':
+        df = df[(df['town'] == town) & (df['year'] == year)]
+    elif town != '':
+        df = df[df['town'] == town]
+    elif year != '':
+        df = df[df['year'] == year]
+    return df.groupby('flat_type')['resale_price'].mean().round(2)
 
 
 def plot_bar_graph(town='', year='', export=False):
     """Call this function to plot bar graph
-    Optional to export graph to pdf
+    Able to save graph as png image
 
     Parameters:
-    town (str): town can be empty if no filtering is selected
-    year (str): year can be empty if no filtering is selected
+    town (str): town can be empty if no filtering is needed
+    year (str): year can be empty if no filtering is needed
     export (bool): pass in True to save graph as pdf
     """
+    try:
+        town = town.upper()
+        df = get_filtered_data(town, int(year))
+        town = 'SINGAPORE' if town == '' else town
+        # Bar graph configurations
+        plt.clf()
+        bargraph = df.plot.barh(color='navy', figsize=(20, 5))
+        bargraph.set_xlabel('Average Resale Value (SGD)')
+        bargraph.set_ylabel('Flat Type')
+        bargraph.set_title('Town: (%s)\nAverage HDB resale value by flat type' % town)
+        if export:
+            plt.savefig('resources/bargraph.png', bbox_inches='tight')
+        plt.show()
+    except ValueError:
+        print("Year is not an integer!")
+    except IndexError:
+        print("No data found!")
 
-    town = town.upper()
-    data = get_data(town, year)
-    town = 'SINGAPORE' if town == '' else town
-    # Bar graph configurations
-    ypos = np.arange(len(data))
-    plt.figure(figsize=(20, 5))
-    plt.barh(ypos, data.values(), color='navy')
-    plt.yticks(ypos, data.keys())
-    plt.ylabel('Average Resale Value (SGD)')
-    plt.title('Town: (%s)\nAverage HDB resale value by flat type' % town)
-    if export:
-        plt.savefig('resources/bargraph.png', bbox_inches='tight')
-    plt.show()
