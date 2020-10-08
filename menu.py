239d06e0d6024de98e4694381ed1e7c29cea3d8f
Belle Sim <bellesimbeier@gmail.com>
2020-10-08 16:42:58 +0800

    Fixing bug

--- menu.py
+++ menu.py
@@ -13,12 +13,8 @@ class WelcomeWindow(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         self.frames = {}
-        # frame = WelcomePage(container, self)
-        # self.frames[WelcomePage] = frame
-        # frame.grid(row=0, column=0, sticky="nsew")
-        # self.show_frame(WelcomePage)
 
-        for F in (WelcomePage, ViewCharts,ViewSummary):
+        for F in (SelectOptions, ViewCharts,ViewSummary):
             frame = F(container, self)
 
             self.frames[F] = frame
@@ -27,37 +23,41 @@ class WelcomeWindow(tk.Tk):
             self.frames[F] = frame
 
             frame.grid(row=0, column=0, sticky="nsew")
-        self.show_frame(WelcomePage)
+        self.show_frame(SelectOptions)
 
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
 
-class WelcomePage(tk.Frame):
+class SelectOptions(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(text="HDB Resale Flats Analyser", font=LARGE_FONT)
-        label.pack()
+        header = tk.Label(text="HDB Resale Flats Analyser", font=LARGE_FONT)
+        label = tk.Label(
+            text="This service enables you to check the resale flat prices within the last 3 years based on regions, towns and flat-types.",
+            font=SMALL_FONT, wraplength=450)
+        header.pack(padx=0, pady=20)
+        label.pack(padx=10, pady=10)
         self.createButtons(controller)
 
     def createButtons(self, controller):
-        chartsBTN = tk.Button(self, text="View Charts", height=5, width=30,
+        chartsBTN = tk.Button(self, text="View Charts", height=5, width=30, font=SMALL_FONT,
                               command=lambda: controller.show_frame(ViewCharts))
-        chartsBTN.pack(pady=0, padx=10)
-        summaryBTN = tk.Button(self, text="View Summary", height=5, width=30,
+        chartsBTN.pack(pady=10, padx=10)
+        summaryBTN = tk.Button(self, text="View Summary", height=5, width=30, font=SMALL_FONT,
                                command=lambda: controller.show_frame(ViewSummary))
-        summaryBTN.pack(pady=0, padx=10)
+        summaryBTN.pack()
 
 
 class ViewCharts(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Analyse Resale Flats by Region", font=NORM_FONT)
-        label.pack(pady=10, padx=10)
+        label.pack(padx=10,pady=10)
 
-        backbutton = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(WelcomePage))
-        backbutton.pack(pady=10, padx=10)
+        backbutton = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(SelectOptions))
+        backbutton.pack(padx=10,pady=10)
 
 
 class ViewSummary(tk.Frame):
@@ -66,11 +66,10 @@ class ViewSummary(tk.Frame):
         label = tk.Label(self, text="Resale Flats Summary", font=NORM_FONT)
         label.pack(pady=10, padx=10)
 
-        backbutton = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(WelcomePage))
+        backbutton = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(SelectOptions))
         backbutton.pack()
 
 
-# Opens the welcome window as soon as this program runs
 app = WelcomeWindow()
 app.title("HDB Resale Flats Analyser")
 app.geometry("600x600")
