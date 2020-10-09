f8800b16ac69b2b2378374242d8ceee246981bfa
Leong Kah En <2002841@sit.singaporetech.edu.sg>
2020-10-09 10:27:00 +0800

    commit display

--- display_data.py
+++ display_data.py
@@ -4,10 +4,12 @@ import seaborn as sns
 import csv_helper
 
 # read the dataset into a data table using Pandas
-df = pd.read_csv("resources/resale_flat_prices.csv")
-df.show()
-# #descriptive statistics summary
-# print(df['resale_price'].describe())
-#
-# print(df.boxplot(column='resale_price'))
-# plt.show()
+df = csv_helper.get_dataframe()
+
+# # Check data types
+# print(df.dtypes)
+
+# display dataset
+print(df)
+
+
