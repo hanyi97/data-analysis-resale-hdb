2c3731d362c4dbdfcbed1ab1722b4f72de54dc17
Leong Kah En <2002841@sit.singaporetech.edu.sg>
2020-10-09 12:54:53 +0800

    modify display function

--- display_data.py
+++ display_data.py
@@ -6,10 +6,16 @@ import csv_helper
 # read the dataset into a data table using Pandas
 df = csv_helper.get_dataframe()
 
-# # Check data types
-# print(df.dtypes)
 
+# Check data types
+print(df.dtypes)
+
+# display dataset function
+def display_dataset():
+    return print(df)
+
+
+# display no. of rows and columns in dataset
+print('This dataset has %d rows and %d columns.' % (df.shape[0], df.shape[1]))
 # display dataset
-print(df)
-
-
+display_dataset()
