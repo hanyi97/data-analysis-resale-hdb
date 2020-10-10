d6d90e29040feb84b0d10a943205b0f4dfcdd491
Leong Kah En <2002841@sit.singaporetech.edu.sg>
2020-10-10 13:34:01 +0800

    update display_data to display in a table

--- display_data.py
+++ display_data.py
@@ -2,23 +2,31 @@
     For user to view everything in the dataset"""
 
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import data_helper
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 
-# read the dataset into a data table using Pandas
-df = data_helper.get_dataframe()
-
-
-# Check data types
-print(df.dtypes)
-
-# Function to display dataset.
-def display_dataset():
-    return print(df)
+from tkinter import *
+from pandastable import Table, TableModel
 
 
-# Display no. of rows and columns in dataset
-print('This dataset has %d rows and %d columns.' % (df.shape[0], df.shape[1]))
-# Call function to display dataset
-display_dataset()
+class Top10ViewWindow(Frame):
+    """Basic test frame for the table"""
+    def __init__(self, parent=None):
+        self.parent = parent
+        Frame.__init__(self)
+        self.main = self.master
+        self.main.geometry('600x400+200+100')
+        self.main.title('HDB Resale Flats Analyser')
+        f = Frame(self.main)
+        f.pack(fill=BOTH,expand=1)
+        df = data_helper.get_dataframe()
+        self.table = pt = Table(f, dataframe=df,
+                                showtoolbar=True, showstatusbar=True)
+        pt.show()
+        return
+
+app = Top10ViewWindow()
+#launch the app
+app.mainloop()
