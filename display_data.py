e6f9db1ea6dc3ca60885c20505017da5d23fcd17
joeychua2000 <1703974D@student.tp.edu.sg>
2020-10-10 13:43:43 +0800

    added docstring and comments. removed check datatype. added get_filtered_data

--- display_data.py
+++ display_data.py
@@ -1,16 +1,10 @@
 """This is a module to display dataset as a summary table
     For user to view everything in the dataset"""
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import data_helper
 
 # read the dataset into a data table using Pandas
 df = data_helper.get_dataframe()
-
-# Check data types
-print(df.dtypes)
 
 
 # Function to display dataset.
@@ -22,3 +16,33 @@ def display_dataset():
 print('This dataset has %d rows and %d columns.' % (df.shape[0], df.shape[1]))
 # Call function to display dataset
 display_dataset()
+
+# user filter selection input
+in_col = {"year": 2019, "town": "YISHUN"}
+
+
+def get_filtered_data(in_col):
+    """Select the data rows according to the user input
+
+    Parameters:
+    in_col dictionary. key = column, value = column data value
+
+    Returns:
+    dataframe: dataframe of filtered results
+    """
+    df = data_helper.get_dataframe()
+    for row in df.iterrows():
+        for column in df.columns:
+            for key in in_col.keys():
+                if column == key:
+                    # get the rows based on the condition in in_col
+                    df = df[df[column].isin(in_col.values())]
+        return df
+
+    # user did not select any inputs to filter the dataframe, return dataframe result
+    if in_col == {}:
+        return df
+
+
+# Call get_filtered_data function to display filtered dataset
+print(get_filtered_data(in_col))
