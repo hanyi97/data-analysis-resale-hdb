da9766543e34fe352b0d750cda9e8be90d7ed7ca
Faiz <faiz89lamps@gmail.com>
2020-10-12 00:21:33 +0800

    Added average resale price and number of flats upon hovering

---
+++ treemap.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import data_helper
+import plotly.express as px
+
+df = pd.read_csv("resources/resale_flat_prices.csv")
+df['all'] = 'Singapore'
+fig = px.treemap(df, path=['all', 'region', 'town','year'],
+    color='resale_price',
+    color_continuous_scale=['#82ff97','#ffcc00', '#ff0000'],
+    maxdepth=2
+    )
+
+fig.data[0].hovertemplate = '%{label}<br>Number of flats: %{value} <br>Average resale price: $%{color:,.0f}'
+fig.show()
+
