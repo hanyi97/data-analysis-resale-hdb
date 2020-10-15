bc966db42de1824d2d95c28b36284668f95b94d2
Faiz <faiz89lamps@gmail.com>
2020-10-15 11:01:15 +0800

    Removed the depth of the graph and colour codes for better visualisation upon export.

--- treemap.py
+++ treemap.py
@@ -6,10 +6,10 @@ df = pd.read_csv("resources/resale_flat_prices.csv")
 df['all'] = 'Singapore'
 fig = px.treemap(df, path=['all', 'region', 'town','year'],
     color='resale_price',
-    color_continuous_scale=['#82ff97','#ffcc00', '#ff0000'],
-    maxdepth=2
+    color_continuous_scale=['#82ff97', '#ff0000'],
     )
 
 fig.data[0].hovertemplate = '%{label}<br>Number of flats: %{value} <br>Average resale price: $%{color:,.0f}'
+fig.write_image("resources/treemap.png")
 fig.show()
 
