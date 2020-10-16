4cabb993b34ea2d392d8d42a4e83ff69feb13411
HanYi <nghanyi1997@gmail.com>
2020-10-16 16:05:45 +0800

    Separated sections into different functions. Allow printing of multiple pages for data summary

--- export.py
+++ export.py
@@ -11,6 +11,7 @@ from reportlab.platypus import Table, TableStyle, Paragraph
 from reportlab.lib import colors
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.units import cm
+
 # Register fonts
 registerFont(TTFont('Arial', 'ARIAL.ttf'))
 registerFont(TTFont('Arial-Bold', 'ARIAL BOLD.ttf'))
@@ -27,38 +28,69 @@ def setup_data_summary_page(pdf):
     Parameters:
     pdf (canvas): canvas object for pdf document
     """
-    df = get_dataframe().head(10)
+    # Retrieve data
+    df = get_dataframe().head(1000)
     data = [df.columns.values.tolist()] + df.values.tolist()
     data[0] = list(map(lambda col_name: col_name.upper().replace('_', ' '), data[0]))
 
     # Page heading configurations
     pdf.setFont('Arial-Bold', 20, None)
     pdf.drawCentredString(415, 550, "Data Summary")
+
+    # Format each text to allow word wrapping
+    pstyle = ParagraphStyle(name='BodyText', fontName='Arial', fontSize=6, wordWrap='CJK')
+    pcol_style = ParagraphStyle(name='BodyText', fontName='Arial-Bold', fontSize=8, wordWrap='CJK')
+    data2 = [[Paragraph(str(row), pcol_style) for row in data[0]]] + \
+            [[Paragraph(str(cell), pstyle) for cell in row] for row in data[1:]]
+
     # Table configurations
-    style = ParagraphStyle(
-        name='BodyText',
-        fontName='Arial',
-        fontSize=8,
-        wordWrap='CJK'
-    )
-    col_style = ParagraphStyle(
-        name='BodyText',
-        fontName='Arial-Bold',
-        fontSize=8,
-        wordWrap='CJK'
-    )
-    data2 = [[Paragraph(str(row), col_style) for row in data[0]]] + \
-            [[Paragraph(str(cell), style) for cell in row] for row in data[1:]]
-    t = Table(data2, colWidths=[50, 50, 100, 50, 60, 50, 100, 50, 50, 50, 50, 50, 50])
-    t.setStyle(TableStyle([('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
-                           ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
-                           ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                           ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-                           ]))
+    col_widths = [50, 50, 70, 50, 60, 50, 100, 50, 40, 80, 50, 50, 50]
+    table_styles = [('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                    ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+                    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                    ('VALIGN', (0, 0), (-1, -1), 'MIDDLE')]
+    x = 15
+    y = 50
     width, height = A4
-    t.wrapOn(pdf, width, height)
-    t.drawOn(pdf, 16, 250)
-    pdf.showPage()
+    data_count = len(data2)
+    # If data is 20 or less, insert table to one page only
+    # 21 because first how is header row
+    if data_count <= 21:
+        # Create table
+        t = Table(data2, colWidths=col_widths)
+        t.setStyle(TableStyle(table_styles))
+        t.wrapOn(pdf, width, height)
+        t.drawOn(pdf, x, y)
+        pdf.showPage()
+    # If more than 20 data
+    else:
+        index = 0
+        end_index = 0
+        rows_per_page = 30
+        while not data_count <= 0:
+            # Add 21 rows to first page (inclusive of header)
+            if index == 0:
+                data3 = data2[:21]
+                index += 21
+                end_index = index + rows_per_page
+                data_count -= 21
+            # Add 20 rows to subsequent pages
+            else:
+                if data_count > rows_per_page:
+                    y = 30
+                    data3 = data2[index:end_index]
+                else:
+                    y = 80
+                    data3 = data2[index:]
+                index = end_index
+                end_index = index + rows_per_page
+                data_count -= rows_per_page
+            # Create table
+            t = Table(data3, colWidths=col_widths)
+            t.setStyle(TableStyle(table_styles))
+            t.wrapOn(pdf, width, height)
+            t.drawOn(pdf, x, y)
+            pdf.showPage()
 
 
 def setup_bargraph_page(pdf):
@@ -73,7 +105,7 @@ def setup_bargraph_page(pdf):
     pdf.drawCentredString(415, 550, "Bar Graph")
     # Insert bar graph image
     if path.isfile(CONST_BARGRAPH_PATH):
-        pdf.drawImage(CONST_BARGRAPH_PATH, x=18, y=280, width=26*cm, height=7*cm)
+        pdf.drawImage(CONST_BARGRAPH_PATH, x=18, y=280, width=26 * cm, height=7 * cm)
     pdf.showPage()
 
 
@@ -101,9 +133,9 @@ def export_to_pdf():
     """
     try:
         pdf = canvas.Canvas(CONST_PDF_PATH, pagesize=landscape(letter))
-        setup_data_summary_page(pdf)
         setup_bargraph_page(pdf)
         setup_treemap_page(pdf)
+        setup_data_summary_page(pdf)
         pdf.save()
     except Exception as e:
         print(e)
