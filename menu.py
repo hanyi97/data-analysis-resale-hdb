51387897be0f6d92ba9dec0a95e6870b56b4e1f2
Belle Sim <bellesimbeier@gmail.com>
2020-10-19 16:30:53 +0800

    Added validation when options are not selected in filter, added new font size

--- menu.py
+++ menu.py
@@ -9,13 +9,14 @@ from pandastable import Table, TableModel
 import bargraph
 import matplotlib
 
-
 matplotlib.use("TkAgg")
 
-#Reusable font sizes
+# Reusable font sizes
 LARGE_FONT = ("Open Sans", 30)
 NORM_FONT = ("Open Sans", 20)
 SMALL_FONT = ("Open Sans", 15)
+SMALLEST_FONT = ("Open Sans", 12)
+
 
 
 # Main Window
@@ -65,6 +66,7 @@ class SelectOptions(tk.Frame):
                                command=lambda: controller.show_frame(ViewSummary))
         summaryBTN.pack()
 
+
 # Export Data Window
 class ExportResults(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -104,23 +106,23 @@ class ViewSummary(tk.Frame):
         backbutton = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(SelectOptions))
         backbutton.pack(padx=5, pady=5)
 
-
-        #get towns, regions,flatTypes data from csv
+        # get towns, regions,flatTypes data from csv
         global df
         df = data_helper.get_dataframe()
         towns = data_helper.get_all_towns()
         regions = data_helper.get_all_regions()
         flatTypes = data_helper.get_all_flatTypes()
 
+        label = tk.Label(self, text="All the fields below are required",
+                                  font=SMALL_FONT)
+        label.pack(padx=20, pady=20)
 
-        selectionlabel = tk.Label(self, text="Please select an option for regions, towns and flat types", font=SMALL_FONT)
-        selectionlabel.pack(padx=20, pady=20)
         # Setting values for regions combo box
         listofRegions = sorted(regions)
         self.comboBoxRegion = ttk.Combobox(self, state="readonly")
         self.comboBoxRegion.pack(padx=5, pady=5)
         self.comboBoxRegion.bind('<<ComboboxSelected>>', lambda x: self.updateComboBox("region"))
-        self.comboBoxRegion['values'] = ["Select Region"]+listofRegions
+        self.comboBoxRegion['values'] = ["Select Region"] + listofRegions
         self.comboBoxRegion.current(0)
 
         # Setting values for town combo box
@@ -128,15 +130,14 @@ class ViewSummary(tk.Frame):
         self.comboBoxTown = ttk.Combobox(self, state="readonly")
         self.comboBoxTown.pack(padx=5, pady=5)
         self.comboBoxTown.bind('<<ComboboxSelected>>', lambda x: self.updateComboBox(""))
-        self.comboBoxTown['values'] = ["Select Town"]+listofTowns
+        self.comboBoxTown['values'] = ["Select Town"] + listofTowns
         self.comboBoxTown.current(0)
-
 
         # Setting values for flat types combo box
         listofFlatTypes = sorted(flatTypes)
         self.comboxBoxFlatTypes = ttk.Combobox(self, state="readonly")
         self.comboxBoxFlatTypes.pack(padx=5, pady=5)
-        self.comboxBoxFlatTypes['values'] = ["Select Flat Type"]+listofFlatTypes
+        self.comboxBoxFlatTypes['values'] = ["Select Flat Type"] + listofFlatTypes
         # self.comboxBoxFlatTypes.set("Select Flat Type")
         self.comboxBoxFlatTypes.current(0)
 
@@ -144,36 +145,39 @@ class ViewSummary(tk.Frame):
         topframe.pack(side=tk.TOP)
 
         filter = tk.Button(self, text="Filter", font=SMALL_FONT,
-                                 command=lambda: self.updateTable(topframe))
+                           command=lambda: self.updateTable(topframe))
         filter.pack(padx=10, pady=10)
 
-        #Creating table for summary
+        # Creating table for summary
         global frame
         frame = tk.Frame(self)
         frame.pack()
-        self.table = Table(frame, dataframe=df,showstatusbar=True)
+        self.table = Table(frame, dataframe=df, showstatusbar=True)
         self.table.show()
 
         exportButton = tk.Button(self, text="Export Results", font=SMALL_FONT,
-                                command=lambda: self.displayExport())
+                                 command=lambda: self.displayExport())
         exportButton.pack(padx=10, pady=10)
 
-
-
-    #resets town dropdown based on region
-    def updateComboBox(self,control):
+    # resets town dropdown based on region
+    def updateComboBox(self, control):
         if control == "region":
             listofTowns = get_town_acrd_region(self.comboBoxRegion.get())
             self.comboBoxTown['values'] = listofTowns
             self.comboBoxTown.current(0)
 
-    def updateTable(self,topframe):
+    def updateTable(self, topframe):
 
         for child in topframe.winfo_children():
             child.destroy()
 
+        #combobox
+        if self.comboBoxRegion.get() == "Select Region" or self.comboBoxTown.get() == "Select Town" or self.comboxBoxFlatTypes == "Select Flat Type":
+            label = tk.Label(topframe, text="Please select an option for region, town and flat type",
+                             font=SMALLEST_FONT, fg="red")
+            label.pack(padx=20, pady=20)
 
-        if not self.comboBoxRegion.get() == "Select Region" or self.comboBoxTown.get() == "Select Town" or self.comboxBoxFlatTypes == "Select Flat Type":
+        else:
             label = tk.Label(self, text="Your Results",
                              font=NORM_FONT)
             label.pack(padx=20, pady=20)
@@ -182,33 +186,33 @@ class ViewSummary(tk.Frame):
             regionLabel.pack()
             townLabel = tk.Label(topframe, text="Town:" + self.comboBoxTown.get())
             townLabel.pack()
-            flatLabel = tk.Label(topframe,text="Flat Type:" +  self.comboxBoxFlatTypes.get())
+            flatLabel = tk.Label(topframe, text="Flat Type:" + self.comboxBoxFlatTypes.get())
             flatLabel.pack()
+
+        if self.comboBoxRegion.get() == "Select Region" or self.comboBoxTown.get() == "Select Town" or self.comboxBoxFlatTypes == "Select Flat Type":
+            self.table = Table(frame, dataframe=df, showstatusbar=True)
+            self.table.show()
+
+
         else:
-            label = tk.Label(topframe, text="Search keywords are empty",
-                             font=SMALL_FONT)
-            label.pack(padx=20, pady=20)
+            # repopulate dictionary for table based on new selected drop down value
+            filters = {"town": self.comboBoxTown.get(), "flat_type": self.comboxBoxFlatTypes.get()}
+            # Replace default values to ""
+            for item in filters:
+                if filters[item] == "Select Town" or filters[item] == "Select Flat Type":
+                    filters[item] = ""
 
-        #repopulate dictionary for table based on new selected drop down value
-        filters = {"town": self.comboBoxTown.get(), "flat_type": self.comboxBoxFlatTypes.get()}
-        # Replace default values to ""
-        for item in filters:
-            if filters[item] == "Select Town" or filters[item] == "Select Flat Type":
-                filters[item] = ""
-
-        #filtered df
-        valuesBasedOnFilters = get_filtered_data(filters)
-        #repopulate table with filtered data
-        self.table.updateModel(TableModel(valuesBasedOnFilters))
-        self.table.redraw()
-
+            # filtered df
+            valuesBasedOnFilters = get_filtered_data(filters)
+            # repopulate table with filtered data
+            self.table.updateModel(TableModel(valuesBasedOnFilters))
+            self.table.redraw()
 
     def displayExport(self):
         mainApp = ExportResults()
         mainApp.title("Export Results")
         mainApp.geometry("800x800")
         mainApp.mainloop()
-
 
 
 # Kah En Function
