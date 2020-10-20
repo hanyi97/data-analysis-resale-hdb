e23311df52b2fb876aeec61cfa9afb128234ce97
Siobhan <bludgeroak@gmail.com>
2020-10-20 16:37:38 +0800

    tree map window will not appear in the correct place

--- menu.py
+++ menu.py
@@ -7,7 +7,11 @@ import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
-
+from tkinter.tix import *
+# from tkinterhtml import HtmlFrame
+# import urllib.request
+from cefpython3 import cefpython as cef
+import sys
 
 LARGE_FONT = ("Open Sans", 30)
 NORM_FONT = ("Open Sans", 20)
@@ -25,7 +29,7 @@ if __name__ == "__main__":
             self.frames = {}
 
             # all pages must be included in this dictionary in order to be raised to the top (accessed)
-            for F in (SelectOptions, ViewCharts, ViewSummary):
+            for F in (SelectOptions, ViewCharts, MainBrowser, ViewSummary):
                 frame = F(container, self)
 
                 self.frames[F] = frame
@@ -69,6 +73,10 @@ if __name__ == "__main__":
                                   command=lambda: controller.show_frame(ViewCharts))
             chartsBTN.pack(pady=10, padx=10)
 
+            treemapBTN = tk.Button(self, text="View Tree Map", height=5, width=30, font=SMALL_FONT,
+                                  command=lambda: controller.show_frame(MainBrowser))
+            treemapBTN.pack(pady=10, padx=10)
+
             # shows the ViewSummary class page upon clicking the button
             summaryBTN = tk.Button(self, text="View Summary", height=5, width=30, font=SMALL_FONT,
                                    command=lambda: controller.show_frame(ViewSummary))
@@ -95,8 +103,27 @@ if __name__ == "__main__":
 
             self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
+        # def onFrameConfigure(self, event):
+        #     '''Reset the scroll region to encompass the inner frame'''
+        #     self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
         def __init__(self, parent, controller):
             tk.Frame.__init__(self, parent)
+
+            # for scrollbar - wrong
+            # self.canvas = tk.Canvas(self, borderwidth=0, background="#ffffff")
+            # self.frame = tk.Frame(self.canvas, background="#ffffff")
+            # self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+            # self.canvas.configure(yscrollcommand=self.vsb.set)
+            #
+            # self.vsb.pack(side="right", fill="y")
+            # self.canvas.pack(side="left", fill="both", expand=True)
+            # self.canvas.create_window((4, 4), window=self.frame, anchor="nw",
+            #                           tags="self.frame")
+            #
+            # self.frame.bind("<Configure>", self.onFrameConfigure)
+
+
             label = tk.Label(self, text="Analyse Resale Flats by Region", font=NORM_FONT)
             label.pack(padx=10, pady=10)
 
@@ -116,8 +143,117 @@ if __name__ == "__main__":
             self.town_combobox.bind("<<ComboboxSelected>>", self.selected)
             self.town_combobox.pack()
 
+            # v = tk.StringVar()
+            # v.set("https://chart-studio.plotly.com/~si00/1.embed")
+            # sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
+            # cef.Initialize()
+            # cef.CreateBrowserSync(url="https://chart-studio.plotly.com/~si00/1.embed",
+            #                       window_title="Tree Map")
+            # cef.MessageLoop()
+
+
+
+            # frame = HtmlFrame(self, horizontal_scrollbar="auto")
+
+        #     # frame.set_content("<iframe width=\"900\" height=\"800\" frameborder=\"0\" scrolling=\"no\" src=\"//plotly.com/~si00/1.embed\"></iframe>")
+        #     frame.set_content("< div > < a  href = \"https://plotly.com/~si00/1/?share_key=aUxlx4CGj6okQIZzfsG4p\" target = \"_blank\" title = \"treemap\"  style = \"display: block; text-align: center;\" > < img src = \"https://plotly.com/~si00/1.png?share_key=aUxlx4CGj6okQIZzfsG4pE\"
+        #     alt = "treemap"
+        #     style = "max-width: 100%;width: 600px;"
+        #     width = "600"
+        #     onerror = "this.onerror=null;this.src='https://plotly.com/404.png';" / > < / a >
+        #     < script
+        #     data - plotly = "si00:1"
+        #     sharekey - plotly = "aUxlx4CGj6okQIZzfsG4pE"
+        #     src = "https://plotly.com/embed.js" async > < / script >
+        #
+        # < / div >")
+
+        #     < div >
+        #     < a
+        #     href = "https://plotly.com/~si00/1/?share_key=aUxlx4CGj6okQIZzfsG4pE"
+        #     target = "_blank"
+        #     title = "treemap"
+        #     style = "display: block; text-align: center;" > < img
+        #     src = "https://plotly.com/~si00/1.png?share_key=aUxlx4CGj6okQIZzfsG4pE"
+        #     alt = "treemap"
+        #     style = "max-width: 100%;width: 600px;"
+        #     width = "600"
+        #     onerror = "this.onerror=null;this.src='https://plotly.com/404.png';" / > < / a >
+        #     < script
+        #     data - plotly = "si00:1"
+        #     sharekey - plotly = "aUxlx4CGj6okQIZzfsG4pE"
+        #     src = "https://plotly.com/embed.js" async > < / script >
+        #
+        # < / div >
+
+        # frame.set_content(urllib.request.urlopen("https://duckduckgo.com").read().decode())
+        #     frame.pack()
+
             backbutton = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(SelectOptions))
             backbutton.pack(padx=10, pady=10)
+
+    class MainBrowser(tk.Frame):
+        def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
+            # self.master.title("Tree Map")
+
+            # Browser
+            self.browser_frame = Browser(self, controller)
+            self.browser_frame.grid(row=1, column=0,
+                                    sticky=(tk.N + tk.S + tk.E + tk.W))
+            tk.Grid.rowconfigure(self, 1, weight=1)
+            tk.Grid.columnconfigure(self, 0, weight=1)
+
+            # self.pack(fill=tk.BOTH, expand=tk.YES)
+
+        def get_browser(self):
+            if self.browser_frame:
+                return self.browser_frame.browser
+            return None
+
+        def get_browser_frame(self):
+            if self.browser_frame:
+                return self.browser_frame
+            return None
+
+        # def embed_browser(self):
+        #     window_info = cef.WindowInfo()
+        #     rect = [0, 0, self.winfo_width(), self.winfo_height()]
+        #     self.browser = cef.CreateBrowserSync(window_info, url="https://chart-studio.plotly.com/~si00/1.embed")
+        #     assert self.browser
+
+            # Pack MainBrowser
+            # self.pack(fill=tk.BOTH, expand=tk.YES)
+
+
+    class Browser(tk.Frame):
+        def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
+            # self.browser=None
+
+        # def embed_browser(self):
+            window_info = cef.WindowInfo()
+        #     rect = [0, 0, self.winfo_width(), self.winfo_height()]
+        #     self.browser = cef.CreateBrowserSync(window_info, url="https://chart-studio.plotly.com/~si00/1.embed")
+        #     assert self.browser
+
+            # self = tk.Tk()
+
+            #using testhtml2-v2.py
+            rect = [0, 0, self.winfo_width(), self.winfo_height()]
+            v = tk.StringVar()
+            v.set("https://chart-studio.plotly.com/~si00/1.embed")
+            sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
+            cef.Initialize()
+            cef.CreateBrowserSync(window_info, url="https://chart-studio.plotly.com/~si00/1.embed",
+                                  window_title="Tree Map")
+            cef.MessageLoop()
+
+
+
+        # def on_configure(self, _):
+        #     if not self.browser:
+        #         self.embed_browser()
 
     # Setting up the ViewSummary page
     class ViewSummary(tk.Frame):
@@ -134,3 +270,4 @@ if __name__ == "__main__":
     app.title("HDB Resale Flats Analyser") #sets title of window
     app.geometry("1920x1080") #sets dimensions of tkinter window
     app.mainloop() #infinite loop so that events get processed
+    cef.Shutdown()
