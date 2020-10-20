cb0cf62b52f500a141024f67f7ba9ad8a90a0faa
Belle Sim <bellesimbeier@gmail.com>
2020-10-20 22:17:55 +0800

    Added top 10 cheapest flats with filters

--- menu.py
+++ menu.py
@@ -5,6 +5,7 @@ import data_helper as dh
 from pandastable import Table, TableModel
 import bargraph
 import matplotlib
+import topNcheapest
 
 matplotlib.use("TkAgg")
 
@@ -13,7 +14,6 @@ LARGE_FONT = ("Open Sans", 30)
 NORM_FONT = ("Open Sans", 16)
 SMALL_FONT = ("Open Sans", 15)
 VALIDAITON_FONT = ("Open Sans", 12)
-
 
 # Main Window
 class WelcomeWindow(tk.Tk):
@@ -24,7 +24,7 @@ class WelcomeWindow(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         self.frames = {}
-        for F in (SelectOptions, ViewCharts, ViewSummary):
+        for F in (SelectOptions, ViewCharts, ViewSummary, ViewTop10CheapestFlats):
             frame = F(container, self)
 
             self.frames[F] = frame
@@ -57,15 +57,18 @@ class SelectOptions(tk.Frame):
     def createButtons(self, controller):
         chartsBTN = tk.Button(self, text="View Charts", height=5, width=30, font=NORM_FONT,
                               command=lambda: controller.show_frame(ViewCharts))
-        chartsBTN.pack(pady=10, padx=10)
-        summaryBTN = tk.Button(self, text="View Summary", height=5, width=30, font=NORM_FONT
-                               ,
+        chartsBTN.pack(padx=10, pady=10)
+        summaryBTN = tk.Button(self, text="View Summary", height=5, width=30, font=NORM_FONT,
                                command=lambda: controller.show_frame(ViewSummary))
-        summaryBTN.pack()
+        summaryBTN.pack(padx=10, pady=10)
+
+        ViewTop10 = tk.Button(self, text="View Top 10 Cheapest Flats", height=5, width=30, font=NORM_FONT,
+                               command=lambda: controller.show_frame(ViewTop10CheapestFlats))
+        ViewTop10.pack(padx=10, pady=10)
 
 
 # Export Data Window
-class ExportResults(tk.Tk):
+class ExportResults(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         label = tk.Label(self, text="Export your results", font=LARGE_FONT)
@@ -92,6 +95,115 @@ class ViewCharts(tk.Frame):
         backbutton = tk.Button(self, text="Back to Home", font=SMALL_FONT,
                                command=lambda: controller.show_frame(SelectOptions))
         backbutton.pack(padx=10, pady=10)
+
+
+
+#Kah En Function
+class ViewTop10CheapestFlats(tk.Frame):
+        def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
+            label = tk.Label(self, text="Top 10 Cheapest Flats", font=LARGE_FONT)
+            label.pack(padx=10, pady=10)
+
+            backbutton = tk.Button(self, text="Back to Home", font=SMALL_FONT,
+                                   command=lambda: controller.show_frame(SelectOptions))
+            backbutton.pack(padx=10, pady=10)
+
+            self.is_table_deleted = False
+
+            # Get regions, towns and flat types from datahelper
+            global df
+            df = dh.get_dataframe()
+            flatTypes = dh.get_all_flatTypes()
+
+            label = tk.Label(self, text="All the fields below are required",
+                             font=NORM_FONT)
+            label.pack(padx=20, pady=20)
+
+            # Setting values for flat types combo box
+            listofFlatTypes = sorted(flatTypes)
+            self.comboxBoxFlatTypes = ttk.Combobox(self, state="readonly")
+            self.comboxBoxFlatTypes.pack(padx=5, pady=5)
+            self.comboxBoxFlatTypes['values'] = ["Select Flat Type"] + listofFlatTypes
+            self.comboxBoxFlatTypes.current(0)
+
+
+            filterButton = tk.Button(self, text="Filter", font=SMALL_FONT,
+                                     command=lambda: self.updateTableAfterFiltering(topframe))
+            filterButton.pack(padx=10, pady=10)
+            # Search results frame
+            topframe = tk.Frame(self)
+            topframe.pack(side=tk.TOP)
+
+            # Plot summary table
+            self.frame = tk.Frame(self)
+            self.frame.pack()
+            self.table = Table(self.frame, dataframe=df)
+            self.table.show()
+
+        def updateTableAfterFiltering(self, topframe):
+            for child in topframe.winfo_children():
+                child.destroy()
+
+            # No options selected, return unfiltered table
+            if self.comboxBoxFlatTypes == "Select Flat Type":
+                label = tk.Label(topframe, text="Please select an option for flat type",
+                                 font=VALIDAITON_FONT, fg="red")
+                label.pack(padx=20, pady=20)
+                self.table = Table(self.frame, dataframe=df)
+                self.table.show()
+
+            # Options selected, return filtered table
+            elif not self.comboxBoxFlatTypes == "Select Flat Type":
+                resultsLabel = tk.Label(topframe, text="Your Results", font=NORM_FONT)
+                resultsLabel.pack()
+                # Return selected option for flat type
+                flatLabel = tk.Label(topframe, text="Flat Type: " + self.comboxBoxFlatTypes.get())
+                flatLabel.pack()
+
+                # Get the filter options from combobox
+                filters = {"flat_type": self.comboxBoxFlatTypes.get()}
+
+                # Replace default values to " "
+                for item in filters:
+                    if  filters[item] == "Select Flat Type":
+                        filters[item] = ""
+
+                # Update df according to updated filtered options
+                global valuesBasedOnFilters
+                valuesBasedOnFilters = topNcheapest.get_filtered_data(filters)
+
+                # Return total number of records for search results
+                global totalRecords
+                totalRecords = str(len(valuesBasedOnFilters))
+
+                totalrowsLabel = tk.Label(topframe, text="Total number of records found: " + totalRecords)
+                totalrowsLabel.pack(padx=10, pady=0)
+
+                # Repopulate table with filtered results
+                if self.is_table_deleted:
+                    self.frame.pack()
+                    self.table = Table(self.frame, dataframe=valuesBasedOnFilters)
+                    self.table.show()
+                    self.exportButton.pack()
+                    self.is_table_deleted = False
+                else:
+                    self.table.updateModel(TableModel(valuesBasedOnFilters))
+                    self.table.redraw()
+
+                # Validation when total records is 0
+                if totalRecords == "0":
+                    del valuesBasedOnFilters
+                    # self.table.clearFormatting()
+                    # self.table.remove()
+                    self.frame.pack_forget()
+                    self.exportButton.pack_forget()
+                    self.is_table_deleted = True
+                    validationLabel = tk.Label(topframe,
+                                               text="Sorry, no matching records found based on filters. Please "
+                                                    "try another search criterion.", font=VALIDAITON_FONT,
+                                               fg="red")
+                    validationLabel.pack()
 
 
 
